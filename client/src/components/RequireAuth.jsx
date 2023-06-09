7b1317dc6767f28a2addb4b9c4d7570e90845c53
Camille Richet <camille.l.a.richet@gmail.com>
2023-06-09 14:23:43 +0100

    fontawesome and axios packages added

--- client/src/components/RequireAuth.jsx
+++ client/src/components/RequireAuth.jsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from "react";
-import AuthContext from "../../context/AuthContext";
+import AuthContext from "./context/AuthContext";
 import { Navigate } from "react-router-dom";
 
 function RequireAuth({ children }) {
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import AuthContext from "../../context/AuthContext";
+import AuthContext from "../components/context/AuthContext";
 import axios from "axios"
-import "./Login.css"
 
 function Login(props) {
 
