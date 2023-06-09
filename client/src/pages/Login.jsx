f89cda00482dc373b089fa9cd36e8fa0204d5d57
Camille Richet <camille.l.a.richet@gmail.com>
2023-06-09 13:43:31 +0100

    login page added

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,73 @@
+import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import AuthContext from "../../context/AuthContext";
+import axios from "axios"
+import "./Login.css"
+
+function Login(props) {
+
+  const auth = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
+  
+  const { email, password } = credentials;
+  
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCredentials({ ...credentials, [name]: value });
+  };
+
+  const login = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios("/api/auth/login", {
+        method: "POST",
+        data: credentials,
+      });
+
+      localStorage.setItem("token", data.token);
+      auth.login();
+      navigate ("/private")
+      console.log(data.message, data.token);
+    } catch (err) {
+      console.log("Error:", err);
+    }
+  };
+
+
+  return (
+    <div className="auth-form-container">
+      <form onSubmit={login} className="login-form">
+        <label htmlFor="email">
+          Email
+        </label>
+        <input
+          value={email}
+          type="email"
+          placeholder="youremail@domain.com"
+          name="email"
+          onChange={handleChange}
+        />
+        <label htmlFor="password">
+          Password
+        </label>
+        <input
+          value={password}
+          name="password"
+          type="password"
+          placeholder="***********"
+          onChange={handleChange}
+        />
+        <button type="submit">Log In</button>
+      </form>
+      <button className="link-btn" onClick={() => props.onFormSwitch("register")}>Don't have an account? Register here.</button>
+    </div>
+  );
+
+}
+
+export default Login;
