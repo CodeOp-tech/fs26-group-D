197a693657d9db0fd1b9b3df735a3021b7e43d4a
Camille Richet <camille.l.a.richet@gmail.com>
2023-06-09 13:41:36 +0100

    Require Auth component done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,61 @@
 import "./App.css";
+import React, { useState, useEffect, useContext } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./assets/Pages/Login";
+import Register from "./assets/Pages/Register";
+import Dashboard from "./assets/Pages/Dashboard";
+import AuthContext from "./context/AuthContext";
+import RequireAuth from "./assets/components/RequireAuth";
 
-export default function App() {
+function App() {
+
+  const [user, setUser] = useState(null);
+  // const auth = useContext(AuthContext);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setUser(true)
+    }
+  }, []);
+
+  function login(email, password) {
+    setUser(true)
+  }
+
+  const logout = () => {
+    setUser(false);
+    localStorage.removeItem("token");
+  };
+
+  const authObject = {
+    user,
+    login,
+    logout,
+  }
+
   return (
-    <div>
-      <h1>Meal Planner App</h1>
-    </div>
+  <AuthContext.Provider value={authObject}>
+      <div>
+        <div>
+          <NavBar />
+        </div>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/private"
+            element={
+              <RequireAuth>
+                <Dashboard />
+              </RequireAuth>
+            }
+          />
+        </Routes>
+      </div>
+    </AuthContext.Provider>
   );
 }
+
+export default App;
---
+++ client/src/components/RequireAuth.jsx
@@ -0,0 +1,35 @@
+import { useContext, useEffect, useState } from "react";
+import AuthContext from "../../context/AuthContext";
+import { Navigate } from "react-router-dom";
+
+function RequireAuth({ children }) {
+  const [loading, setLoading] = useState(true);
+
+  const auth = useContext(AuthContext);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      auth.logout();
+    } else {
+      auth.login();
+    }
+    setLoading(false);
+  });
+
+  if (!auth.user && !loading) {
+    return (
+      <div>
+        <Navigate to="/login" />
+      </div>
+    );
+  } else if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>{children}</div>
+  );
+}
+
+export default RequireAuth;
