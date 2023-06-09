a2bdf20464ddd7259f70fa59b51da6ab3373c5ec
Camille Richet <camille.l.a.richet@gmail.com>
2023-06-09 16:08:19 +0100

    logout added

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import AuthContext from "../components/context/AuthContext";
+import { useNavigate } from "react-router-dom";
 import "../App.css"
 
 function Dashboard() {
 
+  const auth = useContext(AuthContext);
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+
+  const logout = () => {
+    auth.logout();
+    setUser(false);
+    localStorage.removeItem("token");
+    navigate("/");
+  };
+
     return (
       <div>
         <h1>Welcome to your Dashboard</h1>
+        <button onClick={logout} className="logoutBtn">Logout</button>
       </div>
     );
 };
