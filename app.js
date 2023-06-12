82807015d322b349b8cc15355f22d79194f08fd7
Camille Richet <camille.l.a.richet@gmail.com>
2023-06-12 09:54:16 +0100

    installed bcrypt and jwt

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 const createError = require("http-errors");
@@ -14,7 +13,6 @@ app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 // Serve static files from the React frontend app
 app.use(express.static(path.join(__dirname, "client/build")));
