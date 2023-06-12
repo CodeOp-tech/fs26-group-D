0b38d20fce919c971aa21c3cd5e99242c2863e96
Amelie985 <nathernandez985@gmail.com>
2023-06-12 05:16:17 -0500

    corrected routes

--- routes/mealplan.js
+++ routes/mealplan.js
@@ -38,7 +38,11 @@ router.get("/:username/week/:startDate", async (req, res) => {
   try {
     const { username, startDate, hash } = req.params;
 
-    const plan = await spoonacular.getMealPlanForWeek(username, startDate, hash);
+    const plan = await spoonacular.getMealPlanForWeek(
+      username,
+      startDate,
+      hash
+    );
 
     res.json(plan);
   } catch (error) {
@@ -51,25 +55,11 @@ router.get("/:username/week/:startDate", async (req, res) => {
 router.get("/:username/day/:date", async (req, res) => {
   try {
     const { username, date, hash } = req.params;
+    const plan = await spoonacular.getMealPlanForDay(username, date, hash);
 
-    const response = await axios.get(
-      `https://api.spoonacular.com/mealplanner/${username}/day/${date}`,
-      {
-        headers: {
-          "Content-Type": "application/json"
-        },
-        params: {
-          username: username,
-          date: date,
-          hash: hash,
-          apiKey: API_KEY
-        }
-      }
-    );
-
-    res.json(response.data);
+    res.json(plan);
   } catch (error) {
-    console.error("Error retrieving meal plan for week:", error);
+    console.error("Error retrieving meal plan for day:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 });
