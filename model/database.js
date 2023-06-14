1314ff4f80e627e7b4576f3b2634e37b37cc2e92
Camille Richet <camille.l.a.richet@gmail.com>
2023-06-14 10:32:19 +0100

    calendar table added to DB

--- model/database.js
+++ model/database.js
@@ -18,15 +18,14 @@ con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 
-  let sql = "DROP TABLE if exists users; CREATE TABLE users(id INT NOT NULL AUTO_INCREMENT, firstname VARCHAR(60) null, lastname VARCHAR(60) null, email VARCHAR(60) null, password VARCHAR(60) null, PRIMARY KEY (id));";
-  con.query(sql, function (err, result) {
+  let sql =
+    "DROP TABLE if exists users; DROP TABLE if exists calendar; CREATE TABLE users(id INT NOT NULL AUTO_INCREMENT, firstname VARCHAR(60) null, lastname VARCHAR(60) null, email VARCHAR(60) null, password VARCHAR(60) null, PRIMARY KEY (id)); CREATE TABLE calendar(id INT NOT NULL AUTO_INCREMENT, user_id INT DEFAULT 0 NOT NULL, date DATE, meal_type VARCHAR(60), recipe_id INT NOT NULL, recipe_title VARCHAR(255), recipe_image VARCHAR(255), PRIMARY KEY (id))";
+  con.query(sql, function(err, result) {
     if (err) throw err;
-    console.log("Table creation `users` was successful!");
+    console.log("Table creation `users` and `calendar` was successful!");
 
     console.log("Closing...");
   });
 
   con.end();
 });
-
-
