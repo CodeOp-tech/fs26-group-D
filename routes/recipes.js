045803ed8f0ef464270f3111facdd28efc391d96
Camille Richet <camille.l.a.richet@gmail.com>
2023-06-15 16:30:12 +0100

    backend meal edit function created

--- routes/recipes.js
+++ routes/recipes.js
@@ -72,4 +72,18 @@ router.post("/calendar", userShouldBeLoggedIn, async function(req, res) {
   }
 });
 
+router.put("/:meal_id", userShouldBeLoggedIn, async (req, res) => {
+  const { meal_id, recipe_title } = req.params;
+  const { favourite } = req.body;
+
+  try {
+    const results = await db(
+      `UPDATE calendar SET favourite=${favourite} WHERE WHERE id = ${meal_id}; `
+    );
+    res.send({ message: `"${recipe_title}" was added to you calendar` });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
 module.exports = router;
