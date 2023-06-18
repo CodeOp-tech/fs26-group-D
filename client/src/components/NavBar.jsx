f3c330dacd16f1390fda8f95904887fd125d9caa
alxrosc <130445190+alxrosc@users.noreply.github.com>
2023-06-18 18:52:18 +0100

    First draft hero

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -23,100 +23,167 @@ function NavBar() {
 
   return (
     <>
-      <nav className="navbar navbar-expand-md">
-        <div className="container-xxl">
-          <a href="" className="navbar-brand">
-            <span className="fw-bold text-secondary display-5 fs-5">
-              BusyBytes
-            </span>
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#main-nav"
-            aria-controls="main-nav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"> </span>
-          </button>
+      <div className="bg-dark">
+        <div>
+          {!auth.user ? (
+            <nav className="navbar navbar-expand-md p-2 sticky-top bg-dark navbar-dark border border-solid border-secondary rounded border-4">
+              <div className="container-xxl">
+                <a href="#test" className="navbar-brand p-0">
+                  <span className="display-5 fs-5 align-middle">BusyBytes</span>
+                </a>
 
-          <div
-            className="collapse navbar-collapse justify-content-end align-center"
-            id="main-nav"
-          >
-            <ul className="navbar-nav">
-              <li className="nav-item">test</li>
-            </ul>
+                <button
+                  className="navbar-toggler "
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#main-nav"
+                  aria-controls="main-nav"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
+                >
+                  <span className="navbar-toggler-icon"> </span>
+                </button>
+
+                <div
+                  className="collapse navbar-collapse justify-content-end align-center text-white"
+                  id="main-nav"
+                >
+                  <ul className="navbar-nav">
+                    <li className="nav-item">
+                      <button className="btn btn-secondary btn-sm">
+                        Sign In
+                      </button>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
+          ) : (
+            <nav className="navbar navbar-expand-md p-2 sticky-top bg-dark navbar-dark">
+              <div className="container-xxl">
+                <a href="#test" className="navbar-brand p-0">
+                  <span className="display-5 fs-5 align-middle">BusyBytes</span>
+                </a>
+
+                <button
+                  className="navbar-toggler "
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#main-nav"
+                  aria-controls="main-nav"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
+                >
+                  <span className="navbar-toggler-icon"> </span>
+                </button>
+
+                <div
+                  className="collapse navbar-collapse justify-content-end align-center text-white"
+                  id="main-nav"
+                >
+                  {" "}
+                  <div>
+                    <ul>
+                      <li>
+                        <Link className="nav-link" to="/private/dashboard">
+                          Dashboard
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="nav-link" to="/private/mymealplan">
+                          My Meal Plan
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="nav-link" to="/private/shoppinglist">
+                          Shopping List
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="nav-link" to="/private/newmealplan">
+                          New Meal Plan
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="nav-link" to="/private/myfavourites">
+                          My Favourites
+                        </Link>
+                      </li>
+                      <li>
+                        <Link className="nav-link" to="/private/settings">
+                          Settings
+                        </Link>
+                      </li>
+                    </ul>
+                    <button onClick={logout} className="logoutBtn ">
+                      Logout
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </nav>
+          )}
+
+          <div>
+            {auth.user ? (
+              <nav>
+                <div>
+                  <button type="button" onClick={showSidebar}>
+                    <FaBars />
+                  </button>
+                  <div
+                    className={`collapse navbar-collapse ${
+                      isNavOpen ? "show" : "hide"
+                    }`}
+                  >
+                    <div>
+                      <ul>
+                        <li>
+                          <Link className="nav-link" to="/private/dashboard">
+                            Dashboard
+                          </Link>
+                        </li>
+                        <li>
+                          <Link className="nav-link" to="/private/mymealplan">
+                            My Meal Plan
+                          </Link>
+                        </li>
+                        <li>
+                          <Link className="nav-link" to="/private/shoppinglist">
+                            Shopping List
+                          </Link>
+                        </li>
+                        <li>
+                          <Link className="nav-link" to="/private/newmealplan">
+                            New Meal Plan
+                          </Link>
+                        </li>
+                        <li>
+                          <Link className="nav-link" to="/private/myfavourites">
+                            My Favourites
+                          </Link>
+                        </li>
+                        <li>
+                          <Link className="nav-link" to="/private/settings">
+                            Settings
+                          </Link>
+                        </li>
+                      </ul>
+                      <button onClick={logout} className="logoutBtn">
+                        Logout
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </nav>
+            ) : (
+              <div></div>
+            )}
           </div>
         </div>
-      </nav>
+      </div>
     </>
-    // <div>
-    //     { auth.user ? (
-    //     <nav>
-    //         <div>
-    //             <button
-    //                 type="button"
-    //                 onClick={showSidebar}
-    //             ><FaBars/>
-    //             </button>
-    //             <div className={`collapse navbar-collapse ${isNavOpen ? 'show' : 'hide'}`}>
-    //                 <div>
-    //                     <ul>
-    //                         <li>
-    //                             <Link className="nav-link" to="/private/dashboard">
-    //                                 Dashboard
-    //                             </Link>
-    //                         </li>
-    //                         <li >
-    //                             <Link className="nav-link" to="/private/mymealplan">
-    //                                 My Meal Plan
-    //                             </Link>
-    //                         </li>
-    //                         <li>
-    //                             <Link
-    //                                 className="nav-link"
-    //                                 to="/private/shoppinglist"
-    //                             >
-    //                                 Shopping List
-    //                             </Link>
-    //                         </li>
-    //                         <li>
-    //                             <Link
-    //                                 className="nav-link"
-    //                                 to="/private/newmealplan"
-    //                             >
-    //                                 New Meal Plan
-    //                             </Link>
-    //                         </li>
-    //                         <li>
-    //                             <Link
-    //                                 className="nav-link"
-    //                                 to="/private/myfavourites"
-    //                             >
-    //                                 My Favourites
-    //                             </Link>
-    //                         </li>
-    //                         <li>
-    //                             <Link
-    //                                 className="nav-link"
-    //                                 to="/private/settings"
-    //                             >
-    //                                 Settings
-    //                             </Link>
-    //                         </li>
-    //                     </ul>
-    //                     <button onClick={logout} className="logoutBtn">Logout</button>
-    //                 </div>
-    //             </div>
-    //         </div>
-    //         </nav>
-    //         ) : (
-    //             <div></div>
-    //         )}
-    // </div>
+    //
   );
 }
 
--- client/src/pages/Welcome.jsx
+++ client/src/pages/Welcome.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Login from "../components/Login.jsx";
 import Register from "../components/Register.jsx";
 import "../App.css";
+import { Link } from "react-router-dom";
 
 function Welcome() {
   const [currentForm, setCurrentForm] = useState("login");
@@ -13,23 +14,62 @@ function Welcome() {
 
   return (
     <>
-      <div className="container my-5">
-        <h4 className="display-5">Eat, Code, Repeat</h4>
-        <p className="font-monospace">monospace MONOSPACE</p>
-        <p>
+      <section id="hero" className="text-light bg-dark hero-banner">
+        <div className="grid container-lg">
+          <div className="row">
+            <div className="col text-start ">
+              <h1 className="display-3 mt-5 pt-5 mb-3">
+                EAT, <span className="text-secondary">CODE, </span>{" "}
+                <span className="text-primary display-2">REPEAT</span>
+              </h1>
+            </div>
+
+            <div className="col-6 text-center">
+              <div className=" container mt-5 pt-4 col-8">
+                <div className="mt-5 pt-5 text-secondary">
+                  placeholder for image
+                </div>
+              </div>
+              <div className="row mt-5 pt-5 mb-0 pb-0 ">
+                <div className="text-end">
+                  <button className="btn btn-primary btn-lg">
+                    GET STARTED
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="row justify-content-center text-center mt-5">
+            <p className="mb-0 pb-0">Learn more </p>
+
+            <i class="bi bi-caret-down-fill white pt-0"></i>
+          </div>
+        </div>
+      </section>
+
+      {/* <div className="container my-5"> */}
+      {/* <section id="intro">
+<div className="container-lg">
+  <div className="row justify-content-center bg-primary">
+    <div className="text-center">
+    <h2 className="display-5 fs-3">Eat, Code, Repeat</h2>
+      
+      test column
+      
+      
+      </div>
+  </div>
+</div>
+</section> */}
+
+      {/* <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum
           dolor sit amet consectetur adipisicing elit. Alias omnis consectetur
           aperiam dicta itaque iste, nobis nesciunt culpa ratione. Repellat ad
           ut delectus tenetur facere. Ducimus corrupti fugiat sunt quo!
-        </p>
-      </div>
-      <div className="bg-midDark text-light">testing backgrounds</div>
-      <p className="bg-primary">primary</p>
-      <p className="bg-secondary">secondary</p>
-      <p className="bg-dark">Dark</p>
-      <p className="bg-info">Info</p>
-      <p className="bg-midDark">middark</p>
-      <p className="bg-altLight">altLight</p>
+        </p> */}
+      {/* </div> */}
 
       {/* <div>
       <div>
