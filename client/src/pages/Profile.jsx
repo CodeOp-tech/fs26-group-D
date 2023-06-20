d662814ff7b8f85c12badb1370414275ac52d707
Camille Richet <camille.l.a.richet@gmail.com>
2023-06-20 16:58:41 +0100

    settings and favourites view added from profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import MyFavourites from "./MyFavourites";
+import Settings from "./Settings";
 
 export default function Profile() {
   const [user, setUser] = useState({});
   const [error, setError] = useState("");
+  const [showFavourites, setShowFavourites] = useState(false);
+  const [showSettings, setShowSettings] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
 
   useEffect(() => {
     getUserInfo();
@@ -35,18 +40,39 @@ export default function Profile() {
     }
   }
 
+  const handleImageChange = event => {
+    const file = event.target.files[0];
+    setSelectedImage(URL.createObjectURL(file));
+  };
+
   return (
     <>
       <div className="container">
         <div className="row py-3 mt-3 border-bottom border-end border-primary border-3 shadow">
           <div className="col-5  p-3">
             <div className="ps-4 justify-content-center">
-              <img
-                className="shadow img-fluid  mt-1 mb-1 d-block bg-info rounded-circle selected"
-                src="https://i.pinimg.com/736x/44/76/18/447618cb49cf25bccc9ce1c252ca4c5a.jpg"
-                height={150}
-                width={150}
+              <input
+                type="file"
+                accept="image/*"
+                onChange={handleImageChange}
               />
+              {selectedImage || user.profilePicture ? (
+                <img
+                  className="shadow img-fluid mt-1 mb-1 d-block bg-info rounded-circle selected"
+                  src={selectedImage || user.profilePicture}
+                  height={150}
+                  width={150}
+                  alt="Profile"
+                />
+              ) : (
+                <img
+                  className="shadow img-fluid mt-1 mb-1 d-block bg-info rounded-circle selected"
+                  src="https://static.vecteezy.com/system/resources/previews/007/224/792/original/robot-modern-style-vector.jpg"
+                  height={150}
+                  width={150}
+                  alt="Default Profile"
+                />
+              )}
 
               <div key={user.id} className="mt-4">
                 <h2>
@@ -59,7 +85,26 @@ export default function Profile() {
             </div>
           </div>
           <div className="col col-7">
-            SUMMARY OF FAVOURITES AND RESTRICTIONS
+            <div>
+              <button
+                type="button"
+                className="btn btn-link"
+                onClick={() => setShowFavourites(!showFavourites)}
+              >
+                {showFavourites ? "Hide Favourites" : "My Favourites"}
+              </button>
+              {showFavourites && <MyFavourites />}
+            </div>
+            <div>
+              <button
+                type="button"
+                className="btn btn-link"
+                onClick={() => setShowSettings(!showSettings)}
+              >
+                {showSettings ? "Hide Settings" : "My Settings"}
+              </button>
+              {showSettings && <Settings />}
+            </div>
           </div>
         </div>
       </div>
