121179977305732c7119e2e3b79c6f64b20332e5
Germinal Camps <gcampsbarjau@gmail.com>
2023-06-20 11:23:20 +0200

    Germinal messing everything up

--- routes/settings.js
+++ routes/settings.js
@@ -18,45 +18,65 @@ router.get("/restrictions", userShouldBeLoggedIn, async function(req, res) {
   //   .catch((err) => res.status(500).send(err));
 });
 
+// THIS NEEDS REFACTORING
 router.post("/restrictions", userShouldBeLoggedIn, async (req, res) => {
-  const { type, value } = req.body;
+  const { data } = req.body;
+
+  // const data = [
+  //   {
+  //     type: "diet",
+  //     value: "vegan",
+  //   },
+  //   {
+  //     type: "allergies",
+  //     value: "peanuts",
+  //   },
+  //   {
+  //     type: "bad_food",
+  //     value: "mushrooms",
+  //   },
+  // ];
 
   try {
     // await db(
     //   `INSERT INTO settings (user_id, diet, allergies, bad_food) VALUES(${req.user_id}, "${diet}","${allergies}","${bad_food}");`
     // );
-    await Setting.create({
-      user_id: req.user_id,
-      type,
-      value
-    });
+    await Setting.bulkCreate(
+      data.map(setting => ({ ...setting, user_id: req.user_id }))
+    );
     res.send({ message: "Dietary restrictions added" });
   } catch (err) {
     res.status(500).send({ message: err.message });
   }
 });
 
-router.put("/restrictions/:id", userShouldBeLoggedIn, async (req, res) => {
-  const { id } = req.params;
-  const { diet, allergies, bad_food } = req.body;
+router.delete(
+  "/restrictions/:id",
+  userShouldBeLoggedIn,
+  async (req, res) => {}
+);
 
-  try {
-    // await db(
-    //   `UPDATE settings SET diet = "${diet}", allergies = "${allergies}", bad_food = "${bad_food}" WHERE id = ${id};`
-    // );
-    await Setting.update(
-      {
-        diet,
-        allergies,
-        bad_food
-      },
-      { where: { id } }
-    );
-    res.send({ message: "Dietary restrictions updated" });
-  } catch (err) {
-    res.status(500).send({ message: err.message });
-  }
-});
+// router.put("/restrictions/:id", userShouldBeLoggedIn, async (req, res) => {
+//   const { id } = req.params;
+//   const { diet, allergies, bad_food } = req.body;
+
+//   try {
+//     // await db(
+//     //   `UPDATE settings SET diet = "${diet}", allergies = "${allergies}", bad_food = "${bad_food}" WHERE id = ${id};`
+//     // );
+//     await Setting.update(
+//       {
+//         diet,
+//         allergies,
+//         bad_food,
+//       },
+//       { where: { id } }
+//     );
+//     res.send({ message: "Dietary restrictions updated" });
+//   } catch (err) {
+//     res.status(500).send({ message: err.message });
+//   }
+// });
 
 // router.delete(
 //   "/restrictions/:restriction",
